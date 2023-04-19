f69721eb029560f7e5d64bccd04fdddc54fa1c7f
carolinehwang13 <caroline_hwang@brown.edu>
2023-04-19 16:22:55 -0400

    pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,36 @@
 import React from "react";
-import "./App.css";
+import Style from "./src/style/App.css";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import WaitingRoom from "./components/WaitingRoom";
 
+// import * as React from "react";
+// import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+// const root = createRoot(document.getElementById("root"));
+
+// root.render(
+//   <BrowserRouter>{/* The rest of your app goes here */}</BrowserRouter>
+// );
+
 
 function App() {
   return (
-
-    <button onClick=""></button>
-    // <Router>
-    //   <Navbar />
-    //   <Routes>
-    //     <Route exact path="/" exact element={<Home />} />
-    //     <Route path="/about" element={<About />} />
-    //     <Route path="/contact" element={<Contact />} />
-    //     <Route path="/blogs" element={<Blogs />} />
-    //     <Route path="/sign-up" element={<SignUp />} />
-    //   </Routes>
-    // </Router>
+    //<button onClick=""></button>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/waitingroom" element={<WaitingRoom />} />
+          {/* <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/sign-up" element={<SignUp />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- frontend/src/components/WaitingRoom.tsx
+++ frontend/src/components/WaitingRoom.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const WaitingRoom = () => {
   return (
+
+    // <ul>
+    //   <li>
+    //     <Link to="/">WaitingRoom</Link>
+    //   </li>
+    // </ul>
     <div>
       <h1>Waiting Room</h1>
     </div>
