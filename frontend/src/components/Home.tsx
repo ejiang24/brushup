8d4ed870f25f4d0cffddd57bd703fe4556aa99db
cooldog612 <charles_levy@brown.edu>
2023-04-22 16:01:51 -0400

    yas website

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -4,15 +4,26 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
+    <div className="homeVerticalContainer">
       <div className="appTitleContainer">
-        <h1 className="appTitle">Brush Up</h1>
+        <h1 className="appTitle">l3rush Up</h1>
       </div>
-      <button className="joinButton">
-        <li>
-          <Link to="/waitingroom">join room</Link>
-        </li>
-      </button>
+      
+        <input type="text" className="input" name="" id="" />
+
+        <Link to="/waitingroom" className="buttonLink">
+          <button className="joinButton">
+            <div className="buttonText">join room</div>
+          </button>
+        </Link>
+
+
+        <Link to="/waitingroom" className="buttonLink">
+          <button className="createButton">
+            <div className="buttonText">create room</div>
+          </button>
+        </Link>
+   
     </div>
   );
 };
--- frontend/src/components/WaitingRoom.tsx
+++ frontend/src/components/WaitingRoom.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/WaitingRoom.css";
 
 const WaitingRoom = () => {
   return (
