8e8c8afbdb5323406a82370044480fa7a739f7d0
jakobwismar <98495236+jakobwismar@users.noreply.github.com>
2023-05-01 02:42:08 -0400

    Dummy handler integration testing

---
+++ backend/src/test/java/edu/brown/cs32/server/DummyHandlerTest.java
@@ -0,0 +1,58 @@
+package edu.brown.cs32.server;
+
+import edu.brown.cs.student.sprint3.server.records.DummyHandler;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import spark.Spark;
+import spark.utils.IOUtils;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DummyHandlerTest {
+
+    @BeforeAll
+    public static void setUp() throws Exception {
+        DummyHandler handler = new DummyHandler();
+        Spark.port(4567);
+        Spark.post("/dummy", handler);
+        Spark.awaitInitialization();
+    }
+
+    @AfterAll
+    public static void tearDown() throws Exception {
+        Spark.stop();
+    }
+
+    @Test
+    public void testDummyHandler() {
+        TestResponse res = request("POST", "/dummy");
+        assertEquals(200, res.status);
+        assertTrue(res.body.contains("quiz"));
+    }
+
+    private TestResponse request(String method, String path) {
+        try {
+            URL url = new URL("http://localhost:4567" + path);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod(method);
+            connection.setDoOutput(true);
+            connection.connect();
+            String body = IOUtils.toString(connection.getInputStream());
+            return new TestResponse(connection.getResponseCode(), body);
+        } catch (IOException e) {
+            e.printStackTrace();
+            fail("Sending request failed: " + e.getMessage());
+            return null;
+        }
+    }
+
+    private record TestResponse(int status, String body) {
+    }
+}
