17d1529a05dce2a0a4945cc7fee34a9903b4dd7f
jakobwismar <98495236+jakobwismar@users.noreply.github.com>
2023-05-07 18:17:14 -0400

    SaveGameData fuzz testing

--- backend/src/test/java/edu/brown/cs32/server/SaveGameDataTest.java
+++ backend/src/test/java/edu/brown/cs32/server/SaveGameDataTest.java
@@ -1,6 +1,4 @@
 package edu.brown.cs32.server;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import edu.brown.cs.student.sprint3.server.handlers.SaveGameData;
 import org.junit.jupiter.api.Test;
@@ -8,6 +6,12 @@ import org.mockito.Mockito;
 import spark.Request;
 import spark.Response;
 import spark.QueryParamsMap;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SaveGameDataTest {
 
@@ -67,5 +71,33 @@ public class SaveGameDataTest {
         assertTrue(handler.savedScores.containsKey("1"));
         assertTrue(handler.savedScores.get("1").containsKey("1"));
         assertEquals(handler.savedScores.get("1").get("1"), (Integer) 100);
+    }
+
+    @Test
+    public void testFuzzing() {
+        Random rand = new Random();
+
+        // Generate and test 100 random inputs
+        for (int i = 0; i < 100; i++) {
+            // Generate random input values
+            String userID = Integer.toString(Math.abs(rand.nextInt(1000)));
+            String gameID = Integer.toString(Math.abs(rand.nextInt(1000)));
+            int score = Math.abs(rand.nextInt(1000));
+
+            Request request = Mockito.mock(Request.class);
+            Response response = Mockito.mock(Response.class);
+            QueryParamsMap queryParams = Mockito.mock(QueryParamsMap.class);
+
+            Mockito.when(request.queryMap()).thenReturn(queryParams);
+            Mockito.when(queryParams.hasKey("gameID")).thenReturn(true);
+            Mockito.when(queryParams.hasKey("userID")).thenReturn(true);
+            Mockito.when(queryParams.hasKey("score")).thenReturn(true);
+            Mockito.when(queryParams.value("gameID")).thenReturn(gameID);
+            Mockito.when(queryParams.value("userID")).thenReturn(userID);
+            Mockito.when(queryParams.value("score")).thenReturn(Integer.toString(score));
+
+            String result = (String) handler.handle(request, response);
+            assertTrue(result.contains("success"));
+        }
     }
 }
