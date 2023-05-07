a23d39f346eb7ecf751886440e0a1c6b0e5a30de
jakobwismar <98495236+jakobwismar@users.noreply.github.com>
2023-05-07 17:48:08 -0400

    removed broken fuzz test

--- backend/src/test/java/edu/brown/cs32/server/QuizHandlerTest.java
+++ backend/src/test/java/edu/brown/cs32/server/QuizHandlerTest.java
@@ -84,25 +84,4 @@ class QuizHandlerTest {
         }
     }
 
-
-    //Basic fuzz test
-
-//    @Test
-//    public void testFuzz() throws Exception {
-//        Random rand = new Random();
-//        for (int i = 0; i < 100; i++) {
-//            String input = generateInput(rand);
-//            quizHandler.handle(request, response);
-//        }
-//    }
-//
-//    private String generateInput(Random rand) {
-//        // Generate a random input to the QuizHandler
-//        // generate random query parameters for the MET_API_HAS_IMAGES_URL
-//        // For example:
-//        String[] queries = {"painting", "sculpture", "drawing", "photograph", "print"};
-//        String query = queries[rand.nextInt(queries.length)];
-//        return "/quiz?query=" + query;
-//    }
-
 }
